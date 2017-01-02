5d159a10fc10389c16e3c6dfcb635129987cafb0
JinyanLiu <jane.jinyan.liu@gmail.com>
2017-01-02 21:32:51 +0100

    Rewrite query() method in PetProvider.

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -1,10 +1,14 @@
 package com.example.android.pets.data;
 
 import android.content.ContentProvider;
+import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
+
+import com.example.android.pets.data.PetContract.PetEntry;
 
 /**
  * Created by jane on 12/30/16.
@@ -84,7 +88,33 @@ public class PetProvider extends ContentProvider {
     @Override
     public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
                         String sortOrder) {
-        return null;
+
+        SQLiteDatabase database = mDbHelper.getReadableDatabase();
+
+        Cursor cursor;
+
+        int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // Perform a query on the pets table
+                cursor = database.query(
+                        PetEntry.TABLE_NAME,   // The table to query
+                        projection,            // The columns to return
+                        null,                  // The columns for the WHERE clause
+                        null,                  // The values for the WHERE clause
+                        null,                  // Don't group the rows
+                        null,                  // Don't filter by row groups
+                        null);                 // The sort order
+                break;
+            case PET_ID:
+                selection = PetEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(PetEntry.TABLE_NAME, projection, selection, selectionArgs, null, null, sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+        return cursor;
     }
 
     /**
