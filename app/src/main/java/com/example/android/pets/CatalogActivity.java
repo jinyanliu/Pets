c22d428f238edef44ae23717fda9b7900758cf5c
JinyanLiu <jane.jinyan.liu@gmail.com>
2017-01-09 21:01:25 +0100

    Delete all pets from CatalogActivity menu bar. And asking confirmation before deleting.

--- app/src/main/java/com/example/android/pets/CatalogActivity.java
+++ app/src/main/java/com/example/android/pets/CatalogActivity.java
@@ -4,13 +4,16 @@ import android.app.LoaderManager;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.CursorLoader;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -225,6 +228,14 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 values);
     }
 
+    /**
+     * Helper method to delete all pets in the database.
+     */
+    private void deleteAllPets() {
+        int rowsDeleted = getContentResolver().delete(PetEntry.CONTENT_URI, null, null);
+        Log.v("CatalogActivity", rowsDeleted + " rows deleted from pet database");
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu options from the res/menu/menu_catalog.xml file.
@@ -244,7 +255,8 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
                 return true;
             // Respond to a click on the "Delete all entries" menu option
             case R.id.action_delete_all_entries:
-                // Do nothing for now
+//                deleteAllPets();
+                showDeleteConfirmationDialog();
                 return true;
         }
         return super.onOptionsItemSelected(item);
@@ -293,5 +305,34 @@ public class CatalogActivity extends AppCompatActivity implements LoaderManager.
 
         // Callback called when the data needs to be deleted
         mCursorAdapter.swapCursor(null);
+    }
+
+    /**
+     * Prompt the user to confirm that they want to delete all pets.
+     */
+    private void showDeleteConfirmationDialog(){
+        // Create an AlertDialog.Builder and set the message, and click listeners
+        // for the positive and negative buttons on the dialog.
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(R.string.delete_all_dialog_msg);
+        builder.setPositiveButton(R.string.delete, new DialogInterface.OnClickListener(){
+           public void onClick(DialogInterface dialog, int id){
+               // User clicked the "Delete" button, so delete all pets.
+             deleteAllPets();
+           }
+        });
+        builder.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener(){
+           public void onClick(DialogInterface dialog, int id){
+               // User clicked the "Cancel" button, so dismiss the dialog
+               // and continue editing the pet.
+               if (dialog != null){
+                   dialog.dismiss();
+               }
+           }
+        });
+
+        // Create and show the AlertDialog
+        AlertDialog alertDialog = builder.create();
+        alertDialog.show();
     }
 }
